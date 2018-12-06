23d407a80124ec65ae5c0152ae56bd55c11c0db3
Ahmed Abdelgawad <a.abdelgawad@gmail.com>
2018-12-06 17:09:56 +0100

    bulid laravel app as onli API provider

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,52 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::get('/users',function(){
+  $json = [
+    [
+      'id' => '1',
+      'name' => 'ahmed',
+      'age' => '26',
+      'gender' => 'male'
+    ],
+    [
+      'id' => '2',
+      'name' => 'thuan',
+      'age' => '30',
+      'gender' => 'female'
+    ],
+    [
+      'id' => '3',
+      'name' => 'eva',
+      'age' => '26',
+      'gender' => 'female'
+    ]
+  ];
+  return json_encode($json);
+})->middleware('cors');
+Route::get('/users/{id}',function($id){
+  $json = [
+    [
+      'id' => '1',
+      'name' => 'ahmed',
+      'age' => '26',
+      'gender' => 'male'
+    ],
+    [
+      'id' => '2',
+      'name' => 'thuan',
+      'age' => '30',
+      'gender' => 'female'
+    ],
+    [
+      'id' => '3',
+      'name' => 'eva',
+      'age' => '26',
+      'gender' => 'female'
+    ]
+  ];
+  return json_encode($json[$id-1]);
+})->middleware('cors');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
